42359cdbdf725ba557f111c4fc50617e23962e61
Roaau <jorgeroaj411@gmail.com>
2024-12-12 13:12:43 -0600

    Primer commit

---
+++ workexperience/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.conf import settings
+from django.db.models import JSONField
+from django.utils.timezone import now
+
+# Create your models here.
+class WorkExperience(models.Model):
+    position     = models.TextField(default='')
+    company = models.TextField(default='')
+    location = models.TextField(default='')
+    startDate = models.TextField(default=now, blank=True)
+    endDate   = models.TextField(default=now, blank=True)
+    achievements = models.JSONField(blank=True, default=list)
+    posted_by  = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
